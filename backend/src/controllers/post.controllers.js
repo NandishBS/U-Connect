0f08fb6c1091aaffb065d09bd0ac5b3e6d649f12
Nandish BS <nandishbs11@gmail.com>
2024-12-12 00:56:14 +0530

    updated multer and cloudinary

--- backend/src/controllers/post.controllers.js
+++ backend/src/controllers/post.controllers.js
@@ -27,7 +27,7 @@ const uploadPost = asyncHandler(async (req, res) => {
             coverImage = req.files.coverImage[0]
         }
         
-        console.log(content , type, description , content)
+        console.log(content , type, description , author)
 
         if (!(author && type && description && content)) {
             return res.status(400).json(new ApiResponse(400, null, "Missing Credentials : author, posttype, description or content is missing"));
