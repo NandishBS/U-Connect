e232e9dc92c3b5a6379f4c3bc3d938d410d1d89f
Nandish BS <nandishbs11@gmail.com>
2024-12-22 16:41:50 +0530

    fixed the bug in different post opens

--- frontend/src/components/user/ProfilePosts.jsx
+++ frontend/src/components/user/ProfilePosts.jsx
@@ -8,20 +8,22 @@ function ProfilePosts({posts}) {
   posts = posts.filter(post => post.type === 'post')
   const [openPost, setOpenPost] = useState(false)
   const [openEdit, setOpenEdit] = useState(false)
+  const [openedPost, setOpenedPost] = useState(null)
   return (
     <>
       {posts.map((post, index) => <div className='aspect-square w-1/3 p-1 cursor-pointer'>
-        <div onClick={(e)=>{e.stopPropagation();setOpenPost(true)}} className='w-full h-full'>
-          <img src={post.content} className='w-full h-full object-cover' />
-      </div>
+        <div onClick={(e)=>{e.stopPropagation();setOpenedPost(post);setOpenPost(true);
+        }} className='w-full h-full'>
+          <img key={index} src={post.content} className='w-full h-full object-cover' />
       {openPost && <div className='bg-black fixed inset-0 flex justify-center place-items-center ' onClick={(e)=>{
         e.stopPropagation();
         setOpenPost(false)}}>
             <FaXmark onClick={(event)=>{ event.stopPropagation(); setOpenPost(false)}} size={30} className='fixed top-2 right-2 z-20'/>
-            <div onClick={(e)=>{e.stopPropagation()}} className='max-w-96'>
-            <Post post={post}/>
+            <div key={index} onClick={(e)=>{e.stopPropagation()}} className='max-w-96'>
+            <Post key={index} post={openedPost}/>
             </div>
           </div>}
+      </div>
     </div>)}
     </>
   )
